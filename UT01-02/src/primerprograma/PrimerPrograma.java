8f123d21fd868ab544545d8c19830953f3a9835a
José Luis López Álvarez <jllopezalvarez@educa.madrid.org>
2023-09-20 17:55:50 +0200

    Cambio world por DM1E

--- UT01-02/src/primerprograma/PrimerPrograma.java
+++ UT01-02/src/primerprograma/PrimerPrograma.java
@@ -3,7 +3,7 @@ package primerprograma;
 public class PrimerPrograma {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		System.out.println("Hello DM1E");
 	}
 
 }
