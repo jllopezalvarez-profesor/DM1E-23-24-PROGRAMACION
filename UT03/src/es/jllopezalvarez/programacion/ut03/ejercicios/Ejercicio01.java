07a84e5d062449fd5e8091279b961d808dcc43a2
José Luis López Álvarez <jllopezalvarez@educa.madrid.org>
2023-09-28 17:55:07 +0200

    Ejercicio 01

---
+++ UT03/src/es/jllopezalvarez/programacion/ut03/ejercicios/Ejercicio01.java
@@ -0,0 +1,26 @@
+package es.jllopezalvarez.programacion.ut03.ejercicios;
+
+public class Ejercicio01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int numeroEntero = 12;
+		double numeroDecimal = 423.323;
+		String nombre = "José Luis";
+		boolean esEstudiante = false;
+
+		double resultado = numeroEntero * numeroDecimal;
+
+		System.out.println("Valor de numeroEntero: " + numeroEntero);
+
+		System.out.println("Valores de dos numeros: " + (4 + 7));
+
+		System.out.println("Valor de numeroDecimal: " + numeroDecimal);
+		
+		System.out.println("Valor de esEstudiante: " + esEstudiante);
+		
+		
+		
+	}
+
+}
