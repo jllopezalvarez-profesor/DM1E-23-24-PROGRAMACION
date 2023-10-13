b88d291080fa1440c63f74d1a422e6e01483a1dd
José Luis López Álvarez <jllopezalvarez@educa.madrid.org>
2023-10-14 00:54:09 +0200

    Ejercicios UT 04

---
+++ UT04/src/es/jllopezalvarez/programacion/ut04/ejercicios/Ejercicio19.java
@@ -0,0 +1,101 @@
+package es.jllopezalvarez.programacion.ut04.ejercicios;
+
+import java.util.Scanner;
+
+/*
+ * ################################################################################
+ * Enunciado
+ * ################################################################################
+ * Una compañía de transporte internacional tiene servicio en algunos países de 
+ * América del Norte, América Central, América del Sur, Europa y Asia. 
+ * El costo por el servicio de transporte se basa en el peso del paquete y la zona 
+ * a la que va dirigido. Lo anterior se muestra en la tabla:
+ * Zona		Ubicación			Costo/gramo
+ * 1		América del Norte	24.00 euros
+ * 2		América Central		20.00 euros
+ * 3		América del Sur		21.00 euros
+ * 4		Europa				10.00 euros
+ * 5		Asia				18.00 euros
+ * Parte de su política implica que los paquetes con un peso superior a 5 kg no 
+ * son transportados, por cuestiones de logística y de seguridad. Realice un 
+ * algoritmo para determinar el cobro por la entrega de un paquete o, en su caso, 
+ * el rechazo de la entrega.
+ * 
+ * ################################################################################
+ * Análisis
+ * ################################################################################
+ * El costo por el servicio de transporte se basa en el peso del paquete (gramos)
+ * y la zona a la que va dirigido.
+ * Paquetes de más de 5 kg no se transportan.
+ * Datos de entrada:peso en gramos (entero), zona (entero) del 1 al 5
+ * Información de salida: Precio del transporte (real), o mensaje indicando que 
+ * se rechaza la entrega. 
+ *  
+ * ################################################################################
+ * Diseño
+ * ################################################################################
+ * 1. Leer peso
+ * 2. Si no es correcto >0 y <5000 mostrar mensaje de error
+ * 3. Si es correcto:
+ * 4. Leer zona
+ * 5. Según el valor de la zona calculo el coste del transporte.
+ * 6. Mostrar el coste de transporte.
+ * 
+ * ################################################################################
+ */
+public class Ejercicio19 {
+	// Constantes para los importes por zona
+	private static int COSTE_GRAMO_ZONA_1 = 24;
+	private static int COSTE_GRAMO_ZONA_2 = 20;
+	private static int COSTE_GRAMO_ZONA_3 = 21;
+	private static int COSTE_GRAMO_ZONA_4 = 10;
+	private static int COSTE_GRAMO_ZONA_5 = 18;
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		// Preguntamos el peso
+		System.out.print("¿Peso del paquete (en gramos)?: ");
+		int peso = scanner.nextInt();
+
+		// Comprobamos el peso
+		if (peso <= 0) {
+			System.out.println("El peso no puede ser menor o igual que cero.");
+		} else if (peso > 5000) {
+			System.out.println("El peso no puede ser mayor que cinco kilos.");
+		} else {
+			// El peso es válido. Preguntamos la zona.
+			System.out.println("Posibles destinos:");
+			System.out.println("\t1.- América del Norte");
+			System.out.println("\t2.- América Central");
+			System.out.println("\t3.- América del Sur");
+			System.out.println("\t4.- Europa");
+			System.out.println("\t5.- Asia");
+			System.out.print("¿Zona de destino (1-5)? ");
+			int zona = scanner.nextInt();
+
+			// Hacemos un switch para la zona, y si no es una válida mostramos un mensaje de
+			// error.
+			switch (zona) {
+			case 1:
+				System.out.printf("El coste del envío es %d\n", peso * COSTE_GRAMO_ZONA_1);
+				break;
+			case 2:
+				System.out.printf("El coste del envío es %d\n", peso * COSTE_GRAMO_ZONA_2);
+				break;
+			case 3:
+				System.out.printf("El coste del envío es %d\n", peso * COSTE_GRAMO_ZONA_3);
+				break;
+			case 4:
+				System.out.printf("El coste del envío es %d\n", peso * COSTE_GRAMO_ZONA_4);
+				break;
+			case 5:
+				System.out.printf("El coste del envío es %d\n", peso * COSTE_GRAMO_ZONA_5);
+				break;
+			default:
+				System.out.println("Zona no válida");
+				break;
+			}
+		}
+	}
+}
---
+++ UT04/src/es/jllopezalvarez/programacion/ut04/ejercicios/Ejercicio20.java
@@ -0,0 +1,97 @@
+package es.jllopezalvarez.programacion.ut04.ejercicios;
+
+import java.util.Scanner;
+
+/*
+ * ################################################################################
+ * Enunciado
+ * ################################################################################
+ * Escribe un programa en Java que simule una calculadora de operaciones básicas.
+ * El programa debe solicitar al usuario que introduzca dos números y luego le 
+ * dará la opción de realizar una de las siguientes operaciones: 
+ * - suma
+ * - resta
+ * - multiplicación
+ * - división
+ * El usuario debe seleccionar la operación deseada introduciendo un número del 
+ * 1 al 4. Luego, el programa debe realizar la operación seleccionada y mostrar 
+ * el resultado. Si la operación es ilegal (división por cero), debe mostrar un 
+ * mensaje indicando que no se puede realizar.
+ * 
+ * ################################################################################
+ * Análisis
+ * ################################################################################
+ * Hay que pedir los dos operandos.
+ * Una vez solicitados, mostrar las operaciones disponibles, y preguntar al usuario
+ * qué operación quiere realizar. Si introduce una operación no disponible se 
+ * mostrará un mensaje de error.
+ * Si la operación es ilegal (dividir por cero), se mostrará un mensaje de error.
+ * Si todo va bien se mostrará el resultado de la operación
+ * Datos de entrada: operando 1 (double), operando 2 (double), operación (número)
+ * Información de salida: resultado de la operación, o mensaje de error si no se 
+ * ha seleccionado una operación válida o no se puede realizar. 
+ *   
+ * ################################################################################
+ * Diseño
+ * ################################################################################
+ * 1. Leer operanado1 y operando2
+ * 2. Mostrar las operaciones disponibles y preguntar cuál quiere realizar
+ * 3. Si es 1, mostrar la suma
+ * 4. Si es 2, mostrar la resta del primero menos el segundo
+ * 5. Si es 3, mostrar la multiplicación
+ * 6. Si es 4, comprobar si el segundo operando es cero.
+ * 6.1 Si es cero, mostrar mensaje de error
+ * 6.2 Si es distinto de cero, mostrar la división del primero por el segundo.
+ * 7. Si la operación es un valor no válido mostrar mensaje de error.
+ * ################################################################################
+ */
+public class Ejercicio20 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		// Preguntamos el primer operando
+		System.out.print("Primer operando: ");
+		double operando1 = scanner.nextDouble();
+
+		// Preguntamos el segundo operando
+		System.out.print("Segundo operando: ");
+		double operando2 = scanner.nextDouble();
+
+		// Mostramos operaciones
+		System.out.println("Operaciones disponibles: ");
+		System.out.println("1 - Suma");
+		System.out.println("2 - Resta");
+		System.out.println("3 - Multiplicación");
+		System.out.println("4 - División");
+		System.out.print("Operación a realizar: ");
+
+		// Leemos la operación
+		int operacion = scanner.nextInt();
+
+		// Hacemos un switch para la operación, y si no es una válida mostramos un
+		// mensaje de error.
+		switch (operacion) {
+		case 1:
+			System.out.printf("La suma de %f y %f es %f", operando1, operando2, operando1 + operando2);
+			break;
+		case 2:
+			System.out.printf("La resta de %f menos %f es %f", operando1, operando2, operando1 - operando2);
+			break;
+		case 3:
+			System.out.printf("La multiplicación de %f y %f es %f", operando1, operando2, operando1 * operando2);
+			break;
+		case 4:
+			// Comprobamos si el operando 2 es cero.
+			if (operando2 == 0) {
+				System.out.println("No se puede dividir por cero.");
+			} else {
+				System.out.printf("La división de %f entre %f es %f", operando1, operando2, operando1 / operando2);
+			}
+			break;
+		default:
+			System.out.println("Operación no válida");
+			break;
+		}
+	}
+}
