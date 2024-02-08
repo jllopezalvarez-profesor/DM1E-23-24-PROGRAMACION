cc70158f2e8438f78d450068ee777211c9a92d98
José Luis López Álvarez <jllopezalvarez@educa.madrid.org>
2024-02-08 15:51:48 +0100

    Ejercicios

--- UT11/src/es/jllopezalvarez/programacion/ut11/ejercicios/ejercicio22/Camion.java
+++ UT11/src/es/jllopezalvarez/programacion/ut11/ejercicios/ejercicio22/Camion.java
@@ -1,12 +1,31 @@
 package es.jllopezalvarez.programacion.ut11.ejercicios.ejercicio22;
 
-public class Camion extends VehiculoTerrestre{
+public class Camion extends VehiculoTerrestre implements Matriculable {
 
-	public Camion(String nombrePropietario, int numRuedas) {
+	private int cargaMaxima;
+	private String matricula;
+
+	public Camion(String nombrePropietario, int numRuedas, int cargaMaxima, String matricula) {
 		super(nombrePropietario, numRuedas);
 		if (numRuedas < 4 || numRuedas > 16) {
 			throw new IllegalArgumentException("El número de ruedas es incorrecto");
 		}
+		if (cargaMaxima < 0 || cargaMaxima > 40000) {
+			throw new IllegalArgumentException("La carga es incorrecta");
+		}
+
+		this.cargaMaxima = cargaMaxima;
+		this.matricula = matricula;
+	}
+
+	public int getCargaMaxima() {
+		return cargaMaxima;
+	}
+
+	@Override
+	public String getMatricula() {
+		// TODO Auto-generated method stub
+		return this.matricula;
 	}
 
 }
--- UT11/src/es/jllopezalvarez/programacion/ut11/ejercicios/ejercicio22/Ultraligero.java
+++ UT11/src/es/jllopezalvarez/programacion/ut11/ejercicios/ejercicio22/Ultraligero.java
@@ -1,9 +1,18 @@
 package es.jllopezalvarez.programacion.ut11.ejercicios.ejercicio22;
 
-public class Ultraligero extends VehiculoAereo {
+public class Ultraligero extends VehiculoAereo implements Matriculable{
 
-	public Ultraligero(String nombrePropietario, int altitudMaxima) {
+	private String matricula;
+
+	public Ultraligero(String nombrePropietario, int altitudMaxima, String matricula) {
 		super(nombrePropietario, altitudMaxima);
+		this.matricula = matricula;
+	}
+
+	@Override
+	public String getMatricula() {
+		// TODO Auto-generated method stub
+		return this.matricula;
 	}
 
 }
